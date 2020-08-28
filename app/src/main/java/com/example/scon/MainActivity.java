49d81d30ccdc898ad4bf5b7c1e4fe684514a8aa8
yechan0704 <key07040@naver.com>
2020-08-28 16:31:54 +0900

    first_exam

--- app/src/main/java/com/example/scon/MainActivity.java
+++ app/src/main/java/com/example/scon/MainActivity.java
@@ -3,12 +3,36 @@ package com.example.scon;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button name;
+    Button school_number;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        name = findViewById(R.id.name);
+        school_number = findViewById(R.id.school_number);
+
+        name.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "사나이 이예찬", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        school_number.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "20620", Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }

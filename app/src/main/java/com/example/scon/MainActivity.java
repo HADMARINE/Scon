fadbc89b392ea8ab1b7c0581396807d8e90d06e8
yechan0704 <key07040@naver.com>
2020-10-07 12:50:22 +0900

    11차시_exam01

--- app/src/main/java/com/example/scon/MainActivity.java
+++ app/src/main/java/com/example/scon/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.scon;
 
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.net.Uri;
@@ -9,6 +12,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.Switch;
 import android.widget.TextView;
@@ -17,9 +21,11 @@ import android.widget.Toast;
 //20620 이예찬
 
 public class MainActivity extends AppCompatActivity {
-
-    private Button bt_1,bt_2;
-    private EditText et;
+    private Button bt1,bt2;
+    private FrameLayout container;
+    private FragmentTransaction transaction;
+    private FragmentManager fm;
+    private FirstFragment firstFragment = new FirstFragment();
 
 
 
@@ -28,35 +34,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bt_1 = findViewById(R.id.BT_1);
-        bt_2 = findViewById(R.id.BT_2);
-        et = findViewById(R.id.ET);
-
-        bt_1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String asd = et.getText().toString();
-
-                Intent intent1 = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:"+asd));
-                startActivity(intent1);
-            }
-        });
-
-        bt_2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent2 = new Intent(getApplicationContext(),MenuActivity.class);
-
-                intent2.putExtra("name","sunrin");
-                startActivity(intent2);
-                finish();
-            }
-        });
+        bt1 = findViewById(R.id.bt_1);
+        bt2 = findViewById(R.id.bt_2);
+        container = findViewById(R.id.container);
 
 
 
-
-
+    }
+    public void btn1Method(View view){
+        fm = getSupportFragmentManager();
+        transaction = fm.beginTransaction();
+        transaction.add(R.id.container,firstFragment);
+        transaction.commit();
 
     }
 
